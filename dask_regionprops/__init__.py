139b617d15c4745074e94b131550cb97e4521fbd
John Russell <johncrussell25@gmail.com>
2021-12-01 17:52:14 -0500

    update __init__

--- dask_regionprops/__init__.py
+++ dask_regionprops/__init__.py
@@ -5,8 +5,12 @@ except ImportError:
 __author__ = "John Russell"
 __email__ = "johncrussell25@gmail.com"
 
+from .regionprops import regionprops, regionprops_df
+
 __all__ = [
     "__version__",
     "__author__",
     "__email__",
+    "regionprops",
+    "regionprops_df",
 ]
